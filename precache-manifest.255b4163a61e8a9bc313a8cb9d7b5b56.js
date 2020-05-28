8a6778e4d52caac277e3fd74dab70b4de567d1a8
Prajan Acharya <prajanacharya11@gmail.com>
2020-05-28 19:40:28 +0545

    Updates

---
+++ precache-manifest.255b4163a61e8a9bc313a8cb9d7b5b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9342b61d13abc51f6eaea10f10e51208",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "aaf59a30909450c519bc",
+    "url": "/to-do-app-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4723dcfff4d3948f30ab",
+    "url": "/to-do-app-react/static/css/main.bef4af2e.chunk.css"
+  },
+  {
+    "revision": "aaf59a30909450c519bc",
+    "url": "/to-do-app-react/static/js/2.fccec78f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app-react/static/js/2.fccec78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4723dcfff4d3948f30ab",
+    "url": "/to-do-app-react/static/js/main.ec022a34.chunk.js"
+  },
+  {
+    "revision": "88aaff76cda1b07a0627",
+    "url": "/to-do-app-react/static/js/runtime-main.300846d8.js"
+  }
+]);
