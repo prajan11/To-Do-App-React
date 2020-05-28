2189ddf920154eec51c97b1fc1235f3ffd050c13
Prajan Acharya <prajanacharya11@gmail.com>
2020-05-28 19:18:30 +0545

    renaming repo name for deploying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import ToDoForm from "./ToDoForm/ToDoForm.js";
 import ToDoList from "./ToDoList/ToDoList.js";
